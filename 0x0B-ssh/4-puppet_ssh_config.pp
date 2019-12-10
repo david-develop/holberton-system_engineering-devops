858fa7dd01098e8ca522b55256cf1932aad087d5
david-develop <924@holbertonschool.com>
2019-12-10 10:07:49 -0500

    [Fix] 4-puppet config file

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Change the configuration file for not key
 
 exec { 'configfile':
-  command => 'cat "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no\n" >> /etc/ssh/ssh_config',
+  command => 'echo "IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config; echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',
   path    => ['/usr/bin', '/bin'],
 }
