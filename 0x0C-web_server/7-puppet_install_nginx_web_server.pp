8ae6b1f77bbf25353600ce6680534b41c3e5e9e6
david-develop <924@holbertonschool.com>
2019-12-11 17:39:18 -0500

    Fix puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,6 @@
 # Install and configure an Nginx server using Puppet
 
 exec {'update':
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html',
   provider => shell,
-}
-
-exec { 'config redirect':
-  command  => 'redir_str="server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;" ; sudo sed -i "s/server_name _;/$redir_str/" /etc/nginx/sites-available/default ; sudo service nginx restart',
-  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html ; redir_str="server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;" ; sudo sed -i "s/server_name _;/$redir_str/" /etc/nginx/sites-available/default ; sudo service nginx restart',
 }
