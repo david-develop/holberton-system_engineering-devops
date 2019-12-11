9405b5b36c17b4d599cf39fa982ef4e86e1656ab
david-develop <924@holbertonschool.com>
2019-12-11 13:43:04 -0500

    [Fix] puppet script commands

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,14 +1,19 @@
 # Install and configure an Nginx server using Puppet
 
-exec {'update-install':
+exec {'update':
   command  => 'sudo apt-get -y update',
   provider => shell,
 }
 
-package { 'nginx':
-  ensure => installed,
-  name   => 'nginx',
+exec {'install':
+  command  => 'sudo apt-get -y install nginx',
+  provider => shell,
 }
+
+# package { 'nginx':
+#   ensure => installed,
+#   name   => 'nginx',
+# }
 
 exec { 'createfile':
   command  => 'echo "Holberton School" | sudo tee /var/www/html/index.html',
