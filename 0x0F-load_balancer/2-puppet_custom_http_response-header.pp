f3c886627ebe07404adb2eb0bae806e70f0f7851
david-develop <924@holbertonschool.com>
2020-01-03 21:38:41 -0500

    fix puppet script and servers

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,16 +1,6 @@
 # Configure Nginx so that its HTTP response contains a custom header
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx',
-}
-
 exec {'header':
   provider    => shell,
-  command     => 'header_str="location \/ {\n\t\tadd_header X-Served-By \$hostname;" ; sudo sed -i "s/location \/ {/$header_str/" /etc/nginx/sites-available/default',
-}
-
-exec { 'restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+  command     => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; header_str="location \/ {\n\t\tadd_header X-Served-By \$hostname;" ; sudo sed -i "s/location \/ {/$header_str/" /etc/nginx/sites-available/default ; sudo service nginx restart',
 }
