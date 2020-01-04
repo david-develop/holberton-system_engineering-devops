157bd752fe04e435818500fccb4ffdccca23d77a
david-develop <924@holbertonschool.com>
2020-01-03 21:01:38 -0500

    [Update] puppet script

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,12 +1,17 @@
 # Configure Nginx so that its HTTP response contains a custom header
 
-exec {'update':
+exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html ; redir_str="server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;" ; sudo sed -i "s/server_name _;/$redir_str/" /etc/nginx/sites-available/default',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx',
 }
 
-exec {'headernginx':
+exec {'header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
-  command     => 'header_str="location \/ {\n\t\tadd_header X-Served-By \"$HOST\";" ; sudo sed -i "s/location \/ {/$header_str/" /etc/nginx/sites-available/default ; sudo service nginx restart',
+  command     => 'header_str="location \/ {\n\t\tadd_header X-Served-By \"$HOST\";" ; sudo sed -i "s/location \/ {/$header_str/" /etc/nginx/sites-available/default',
+}
+
+exec { 'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
